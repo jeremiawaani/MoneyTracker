66105a47c880b805f06a2d4ed70d1dbfbcd58b81
Jeremia Waani <s21910028@student.unklab.ac.id>
2022-05-15 12:04:04 +0800

    upload folder Money Tracker

---
+++ src/pages/HomeIdk.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+
+export default function HomeIdk() {
+    return(
+        <View>
+            <Text style={{textAlign: 'center', paddingTop: 300, backgroundColor: 'white'}}>Transaksi</Text>
+            <View style={{backgroundColor: 'white', height: 300}}/>
+        </View>
+    );
+}
